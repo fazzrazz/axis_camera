8a186f6dab7a7dd14e9668eb2820f6db39e4eeb4
fazzrazz <danielduranrojas@gmail.com>
2023-08-25 14:47:57 -0400

    adds ROS 2 support

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
+import os
+from glob import glob
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    install_requires=['rospy', 'urllib'],
-    )
+package_name = 'axis_camera'
 
-setup(**d)
+setup(
+    name=package_name,
+    version='0.4.3',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Chris Iverach-Brereton',
+    maintainer_email='civerachb@clearpathrobotics.com',
+    description='Python ROS 2 drivers for accessing an Axis camera\'s MJPG stream. Also provides control for PTZ cameras.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'resource = lrs_exec.resource:main'
+        ]
+    }
+    
+)
